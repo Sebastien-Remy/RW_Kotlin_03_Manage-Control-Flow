56910a1f0a9b992268baa44468c5441a5ef52277
Sebastien REMY <seb.remy@me.com>
2021-04-18 10:32:02 +0200

    While loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val visitedCountries = arrayOf(
+        "Costa Rica", "Netherland", "USA", "Spain"
+    )
+
+    // While loop
+    var i = 0
+    while (i < visitedCountries.size) {
+        println ("Contry at index $i: ${visitedCountries[i]}")
+        i +=1
+    }
+
+    i = 0
+
+    // Do while
+    do {
+        println ("Contry at index $i: ${visitedCountries[i]}")
+        i +=1
+    } while (i < visitedCountries.size)
+
+
 }
